11a93f712f65fbd90213ce541e35f607ec3c0cff
divo <sdiviney@zendesk.com>
2019-12-03 20:11:44 -0800

    Basic image sort working:

--- jruby/image_sort.rb
+++ jruby/image_sort.rb
@@ -8,11 +8,38 @@ class TemplateSketch < Propane::App
   end
 
   def setup
-    sketch_title 'Template'
+    sketch_title 'Image sort'
     @img = load_image(data_path('image.jpg'))
   end
 
   def draw
+    tile_count = width / [mouseX, 1].max
+    #tile_count = width / [mouseX, 5].max
+    rect_size = width / tile_count # Do I need to make this a float explicitly
+
+    colors = []
+
+    # scan
+    for y in (0..tile_count)
+      for x in (0..tile_count)
+        px = x * rect_size
+        py = y * rect_size
+        # Sort entire image, then later do sort row by row
+        colors << @img.get(px, py)
+      end
+    end
+
+    colors.sort!
+
+    # draw
+    i = 0
+    for y in (0..tile_count)
+      for x in (0..tile_count)
+        fill colors[i]
+        rect(x * rect_size, y * rect_size, rect_size, rect_size)
+        i += 1
+      end
+    end
   end
 end
 
