df60493e9b136cc3aa0f422f5d33e90646145e10
divo <sdiviney@zendesk.com>
2019-12-08 19:32:23 -0800

    Small cleanup of dumb_agents

--- jruby/dumb_agents.rb
+++ jruby/dumb_agents.rb
@@ -17,43 +17,55 @@ class DumbAgents < Propane::App
     @pos_x, @pos_y = rand(width), rand(height) # Set these to random values?
   end
 
+  def step_size
+    1
+  end
+
+  def diameter
+    1
+  end
+
 
   def draw
-    step_size = 1
-    diameter = 1
     for x in (0..mouseX)
       direction = rand(8)
-      case direction
-      when 0
-        @pos_y -= step_size
-      when 1
-        @pos_x += step_size
-        @pos_y -= step_size
-      when 2
-        @pos_x += step_size
-      when 3
-        @pos_x += step_size
-        @pos_y += step_size
-      when 4
-        @pos_y += step_size
-      when 5
-        @pos_x -= step_size
-        @pos_y += step_size
-      when 6
-        @pos_x -= step_size
-      when 7
-        @pos_x -= step_size
-        @pos_y -= step_size
-      end
-
-      @pos_x = 0 if @pos_x > width
-      @pos_x = width if @pos_x < 0
-      @pos_y = height if @pos_y < 0
-      @pos_y = 0 if @pos_y > height
-
+      step(direction)
+      wrap
       fill(0, 40)
       ellipse(@pos_x + step_size / 2, @pos_y + step_size / 2, diameter, diameter)
     end
+  end
+
+  def step(direction)
+    case direction
+    when 0
+      @pos_y -= step_size
+    when 1
+      @pos_x += step_size
+      @pos_y -= step_size
+    when 2
+      @pos_x += step_size
+    when 3
+      @pos_x += step_size
+      @pos_y += step_size
+    when 4
+      @pos_y += step_size
+    when 5
+      @pos_x -= step_size
+      @pos_y += step_size
+    when 6
+      @pos_x -= step_size
+    when 7
+      @pos_x -= step_size
+      @pos_y -= step_size
+    end
+  end
+
+  def wrap
+    @pos_x = 0 if @pos_x > width
+    @pos_x = width if @pos_x < 0
+    @pos_y = height if @pos_y < 0
+    @pos_y = 0 if @pos_y > height
   end
 end
 
