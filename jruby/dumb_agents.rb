72f7ace7692c34ae83587b53d76e2118b2018974
divo <sdiviney@zendesk.com>
2019-12-08 19:56:42 -0800

    More controls, mode 3 doesn't seem to be drawing every 10 frames?

--- jruby/dumb_agents.rb
+++ jruby/dumb_agents.rb
@@ -4,35 +4,53 @@ require 'propane'
 
 class DumbAgents < Propane::App
 
+  BACKGROUND = 255
+
+  attr_accessor :step_size
+  attr_accessor :diameter
+  attr_accessor :mode
+  attr_accessor :paused
+
   def settings
-    #size(2000, 1500)
-    size(800, 800)
+    size(2000, 1500)
   end
 
   def setup
     sketch_title 'DumbAgents'
-    background(255)
+    background(BACKGROUND)
     smooth()
     noStroke()
+    @step_size = 1
+    @diameter = 1
+    @mode = 1
+    @paused = false
+    @counter = 0
 
     @pos_x, @pos_y = rand(width), rand(height) # Set these to random values?
   end
 
-  def step_size
-    1
-  end
-
-  def diameter
-    1
-  end
-
-
   def draw
+    return if @paused
+    @counter += 1
+
     for x in (0..mouseX)
-      #direction = rand(8)
-      direction = rand(1..5)
+      if mode == 2
+        direction = rand(0..2)
+      else
+        direction = rand(0..7)
+      end
+
       step(direction)
       wrap
+
+      if mode == 3
+        if @counter >= 10
+          @counter = 0
+          fill(192, 100, 64, 80)
+          ellipse(@pos_x + step_size / 2, @pos_y + step_size / 2, diameter + 7, diameter + 7)
+        end
+      end
+
       fill(0, 40)
       ellipse(@pos_x + step_size / 2, @pos_y + step_size / 2, diameter, diameter)
     end
@@ -68,6 +86,27 @@ class DumbAgents < Propane::App
     @pos_x = width if @pos_x < 0
     @pos_y = height if @pos_y < 0
     @pos_y = 0 if @pos_y > height
+  end
+
+  def keyReleased
+    case key
+    when 'd'
+      background(BACKGROUND)
+    when '1'
+      @mode = 1
+      @step_size = 1
+      @diameter = 1
+    when '2'
+      @mode = 2
+      @step_size = 1
+      @diameter = 1
+    when '3'
+      @mode = 3
+      @step_size = 10
+      @diameter = 5
+    when 'p'
+      @paused = !@paused
+    end
   end
 end
 
