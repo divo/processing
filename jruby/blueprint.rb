8b8248221b72956d081b295082589f00637b2bac
divo <sdiviney@zendesk.com>
2019-12-14 22:12:15 -0800

    Basic drawing working

--- jruby/blueprint.rb
+++ jruby/blueprint.rb
@@ -3,6 +3,7 @@ require 'propane'
 
 class Blueprint < Propane::App
 
+  # TOOD: WTF exaclty do these accessors do
   attr_accessor :centerX, :centerY
   attr_accessor :zoom
   attr_accessor :text # TODO: Option to save this
@@ -14,7 +15,7 @@ class Blueprint < Propane::App
 
   def settings
     size(800, 600)
-    centerX, @centerY = width / 2, height / 2 # TODO: Mouse clicked
+    @centerX, @centerY = width / 2, height / 2 # TODO: Mouse clicked
     zoom = 1 # TODO: Scaling
     @text = ''
   end
@@ -35,14 +36,17 @@ class Blueprint < Propane::App
 
   def draw
     # TODO: scaling and translation are kinda key to this
-    #translate(centerX, centerY) # Move to mouse position
+    translate(centerX, centerY) # Move to mouse position
     #scale(zoom)
 
     text_font(@font)
     text.chars.each do |char|
       char_width = text_width(char)
-
-
+      case char
+      when ' '
+        shape(shape_space, 0, 0)
+        translate(10.9, 0)
+      end
     end
   end
 
@@ -50,9 +54,9 @@ class Blueprint < Propane::App
   def keyPressed
 		case key
 		when BACKSPACE
-			text = text.delete_suffix!(text.chars.last)
+			@text = text.delete_suffix!(text.chars.last)
 		else
-			text += key
+			@text += key
 		end
   end
 end
